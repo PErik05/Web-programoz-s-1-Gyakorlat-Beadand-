6f5c9e3670acd8d3d3f84e038ad745a28d888be2
tobega2002 <tobega2002@gmail.com>
2026-05-06 17:07:53 +0200

    Update crud.tpl.php

--- templates/pages/crud.tpl.php
+++ templates/pages/crud.tpl.php
@@ -1,6 +1,5 @@
 <h1 style="text-align:center;">CRUD OPERATIONS</h1>
 
-<!-- ADD USER FORM -->
 <h3>Add User</h3>
 
 <form method="post" action="?crud_add">
@@ -13,10 +12,9 @@
 
 <hr>
 
-<!-- TABLE -->
 <table>
     <tr>
-        <th>id</th>
+        <th>ID</th>
         <th>Name</th>
         <th>Email</th>
         <th>Mobile</th>
@@ -24,32 +22,22 @@
     </tr>
 
 <?php
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=gyakorlat7', 'root', '',
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+// 🔥 EZ FONTOS: config.inc.php-ból jön $dbh
+$stmt = $dbh->query("SELECT * FROM users ORDER BY id DESC");
 
-    $dbh->query('SET NAMES utf8');
+foreach ($stmt as $row) {
+    echo "<tr>";
+    echo "<td>{$row['id']}</td>";
+    echo "<td>{$row['name']}</td>";
+    echo "<td>{$row['email']}</td>";
+    echo "<td>{$row['mobile']}</td>";
 
-    $sql = "SELECT * FROM users ORDER BY id DESC";
+    echo "<td>
+            <a class='btn-blue' href='?crud_edit={$row['id']}'>Edit</a>
+            <a class='btn-red' href='?crud_delete={$row['id']}'>Delete</a>
+          </td>";
 
-    foreach ($dbh->query($sql) as $row) {
-
-        echo "<tr>";
-        echo "<td>{$row['id']}</td>";
-        echo "<td>{$row['name']}</td>";
-        echo "<td>{$row['email']}</td>";
-        echo "<td>{$row['mobile']}</td>";
-
-        echo "<td>
-                <a class='btn-blue' href='?crud_edit={$row['id']}'>Edit</a>
-                <a class='btn-red' href='?crud_delete={$row['id']}'>Delete</a>
-              </td>";
-
-        echo "</tr>";
-    }
-
-} catch (PDOException $e) {
-    echo "Hiba: " . $e->getMessage();
+    echo "</tr>";
 }
 ?>
 
